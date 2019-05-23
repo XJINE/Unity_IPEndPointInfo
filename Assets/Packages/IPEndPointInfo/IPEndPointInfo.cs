537d2c0cd099f5422ec451afd7834575d88c203a
XJINE <xjine.andorelseif@gmail.com>
2019-05-23 20:53:10 +0900

    First commit.

---
+++ Assets/Packages/IPEndPointInfo/IPEndPointInfo.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+
+[System.Serializable]
+public class IPEndPointInfo
+{
+    #region Field
+
+    public string address;
+    public int    port;
+
+    #endregion Field
+
+    #region Property
+
+    protected IPEndPoint ipEndPoint;
+
+    public IPEndPoint IPEndPoint
+    {
+        get
+        {
+            if (this.ipEndPoint == null)
+            {
+                this.ipEndPoint = new IPEndPoint(FindIPAddress(this.address), this.port);
+            }
+
+            return this.ipEndPoint;
+        }
+    }
+
+    #endregion Property
+
+    #region Method
+
+    public static IPAddress FindIPAddress(string hostname)
+    {
+        IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+        IPAddress   address   = IPAddress.None;
+
+        for (var i = 0; i < addresses.Length; i++)
+        {
+            if (addresses[i].AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                address = addresses[i];
+                break;
+            }
+        }
+
+        return address;
+    }
+
+    #endregion Method
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Unity_IPEndPointInfo
+
 IPEndPoint wrapper to setup from Inspector.
+
+## Import to Your Project
+
+You can import this asset from UnityPackage.
+
+- [IPEndPointInfo.unitypackage](https://github.com/XJINE/Unity_IPEndPointInfo/blob/master/IPEndPointInfo.unitypackage)
+
+## How to Use
+
+Set ``address`` and ``port`` field. And get ``IPEndPoint`` from property.
