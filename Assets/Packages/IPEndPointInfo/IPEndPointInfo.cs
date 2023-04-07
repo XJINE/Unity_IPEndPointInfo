f18311a230376db245dc0b64e2eaefc5de5f0c4e
XJINE <xjine.andorelseif@gmail.com>
2023-04-07 18:42:39 +0900

    Enable to update address and port by using properties.

--- Assets/Packages/IPEndPointInfo/IPEndPointInfo.cs
+++ Assets/Packages/IPEndPointInfo/IPEndPointInfo.cs
@@ -1,20 +1,47 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using UnityEngine;
 
-[System.Serializable]
+[Serializable]
 public class IPEndPointInfo
 {
     #region Field
 
-    public string address;
-    public int    port;
+    public static readonly IPAddress LocalIPAddress = IPAddress.Parse("127.0.0.1");
+
+    [SerializeField] private string address;
+    [SerializeField] private int    port;
 
     #endregion Field
 
     #region Property
 
     private IPEndPoint _ipEndPoint;
+    private IPEndPoint _ipEndPointLocal;
 
-    public IPEndPoint IPEndPoint => _ipEndPoint ??= new IPEndPoint(IPAddress.Parse(address), port);
+    public string Address
+    {
+        get => address;
+        set
+        {
+            address          = value;
+            _ipEndPoint      = new IPEndPoint(IPAddress.Parse(address), port);
+            _ipEndPointLocal = new IPEndPoint(LocalIPAddress,           port);
+        }
+    }
+    public int Port
+    {
+        get => port;
+        set
+        {
+            port             = value;
+            _ipEndPoint      = new IPEndPoint(IPAddress.Parse(address), port);
+            _ipEndPointLocal = new IPEndPoint(LocalIPAddress,           port);
+        }
+    }
+
+    public IPEndPoint IPEndPoint      => _ipEndPoint      ??= new IPEndPoint(IPAddress.Parse(address), port);
+    public IPEndPoint IPEndPointLocal => _ipEndPointLocal ??= new IPEndPoint(LocalIPAddress,           port);
 
     #endregion Property
 
